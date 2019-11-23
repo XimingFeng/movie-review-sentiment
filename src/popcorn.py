1c40937d73df1ec54c462753f73579edaa52bd67
XimingFeng <xfeng36@asu.edu>
2019-11-23 16:47:51 -0700

    add stem function

--- src/popcorn.py
+++ src/popcorn.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 import re
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
 class PopcornHelper():
     
@@ -17,16 +19,23 @@ class PopcornHelper():
         train_data = self.read_tsv_file(folder_path + "labeledTrainData.tsv")
         test_data = self.read_tsv_file(folder_path + "testData.tsv")
         return train_data, test_data
-    
+
+
+    def clean_word(self, word):
+        poter_stemmer = PorterStemmer()
+        word = re.sub(r"<br />", " ", word)
+        word = re.sub(r"[^a-z]", " ", word)
+        word = re.sub(r"   ", " ", word)  # Remove any extra spaces
+        word = re.sub(r"  ", " ", word)
+        word = poter_stemmer.stem(word)
+        return word
+
     def clean_text(self, text, remove_stopwords=True):
-        text_list = text.lower().split()
+        text_list = word_tokenize(text)
         if remove_stopwords:
             stop_wds = set(stopwords.words("english"))
-            text_list = [w for w in text_list]
+            text_list = [self.clean_word(w) for w in text_list if not w in stop_wds]
+
         text = " ".join(text_list)
-        text = re.sub(r"<br />", " ", text)
-        text = re.sub(r"[^a-z]", " ", text)
-        text = re.sub(r"   ", " ", text) # Remove any extra spaces
-        text = re.sub(r"  ", " ", text)
         return text
-    
+
