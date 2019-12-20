8205a0fc3c63fef818a4c36f3f29bebe675268be
XimingFeng <xfeng36@asu.edu>
2019-12-19 20:06:51 -0700

    add predict function function

--- src/lstm.py
+++ src/lstm.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from src.popcorn import PopcornHelper
 import os
+import numpy as np
 
 class lstm_classifier():
 
@@ -15,10 +16,13 @@ class lstm_classifier():
         self.model_dir = self.root_dir + "/model/basic_lstm/"
         self.verbose = verbose
         self.train = None
+        self.scores = None
         self.accuracy = None
         self.loss = None
+        self.confidence = None
         self.X = None
         self.y_true = None
+        self.is_training = None
         tf.reset_default_graph()
         self.graph = tf.get_default_graph()
         self.data_helper = None
@@ -27,6 +31,7 @@ class lstm_classifier():
         self.keep_prob = None
         self.wd_dict_size = len(self.data_helper.corpora_dict)
         self.sess = tf.Session()
+        self.batch_size = batch_size
         if not os.path.exists(self.model_dir+"basic_lstm.meta"):
             print("No meta graph found. Start to build graph from scratch")
             self.initialize_graph()
@@ -35,11 +40,30 @@ class lstm_classifier():
             self.restore_model()
             print("Graph built from meta file")
 
+    def new_weight_variable(self, shape, name):
+        initial = tf.truncated_normal_initializer(stddev=0.01)
+        return tf.get_variable(dtype=tf.float32, shape=shape, initializer=initial, name=name + "_W")
+
+    def new_bias_variable(self, shape, name):
+        initial = tf.constant(0., shape=shape, dtype=tf.float32)
+        return tf.get_variable(dtype=tf.float32, initializer=initial, name=name + "_b")
+
+    def fully_conn_layer(self, input_X, output_dim, output_name, use_relu=True):
+        input_dim = input_X.get_shape()[1]
+        W = self.new_weight_variable([input_dim, output_dim], output_name)
+        b = self.new_bias_variable([output_dim], output_name)
+        affine_out = tf.matmul(input_X, W) + b
+        layer_output = affine_out
+        if use_relu:
+            layer_output = tf.nn.relu(affine_out)
+        return tf.identity(layer_output, name=output_name)
+
     def initialize_graph(self):
         with tf.name_scope("input"):
             self.X = tf.placeholder(dtype=tf.int32, shape=[None, self.num_wds], name="X")
             self.y_true = tf.placeholder(dtype=tf.int32, shape=[None, self.class_num], name="y_true")
             self.keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+            self.is_training = tf.placeholder(tf.bool, name="is_training")
 
         with tf.name_scope("embedding"):
             embed_lookup = tf.Variable(tf.random_normal(shape=[self.wd_dict_size, self.embed_size]))
@@ -52,24 +76,29 @@ class lstm_classifier():
             rnn_final_out = outputs[:, -1]
 
         with tf.name_scope("feed_forward_layer"):
-            full_conn1 = tf.layers.dense(rnn_final_out, self.dense_size[0], activation=tf.nn.relu)
-            full1_batch_norm = tf.layers.batch_normalization(full_conn1, training=True)
-            full_conn2 = tf.layers.dense(full1_batch_norm, self.dense_size[1], activation=tf.nn.relu)
-            full2_batch_norm = tf.layers.batch_normalization(full_conn2, training=True)
-            scores = tf.layers.dense(full2_batch_norm, self.class_num, activation=None, name="scores")
+            full_conn1 = self.fully_conn_layer(rnn_final_out, self.dense_size[0], output_name="full_conn1")
+            full1_batch_norm = tf.layers.batch_normalization(full_conn1, training=self.is_training)
+            full_conn2 = self.fully_conn_layer(full1_batch_norm, self.dense_size[1], output_name="full_conn2")
+            full2_batch_norm = tf.layers.batch_normalization(full_conn2, training=self.is_training)
+
+        with tf.name_scope("scores_output"):
+            self.scores = self.fully_conn_layer(full2_batch_norm, self.class_num, output_name="scores", use_relu=False)
+            self.confidence = tf.nn.softmax(logits=self.scores, name="confidence")
 
         with tf.name_scope("cross_entropy"):
-            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=self.y_true), name="loss")
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.y_true), name="loss")
 
         with tf.name_scope("train"):
             optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
             self.train = optimizer.minimize(self.loss, name="train")
 
         with tf.name_scope("accuracy"):
-            matches = tf.equal(tf.argmax(scores, axis=1), tf.argmax(self.y_true, axis=1))
+            matches = tf.equal(tf.argmax(self.scores, axis=1), tf.argmax(self.y_true, axis=1))
             self.accuracy = tf.reduce_mean(tf.cast(matches, tf.float32), name="accuracy")
         self.sess.run(tf.global_variables_initializer())
         tf.add_to_collection("train", self.train)
+
+
 
     def setup_data(self, batch_size):
         self.data_helper = PopcornHelper(self.root_dir, batch_size=batch_size)
@@ -77,45 +106,80 @@ class lstm_classifier():
         self.data_helper.pad_train_test(max_len=self.num_wds)
         self.data_helper.set_train_validation(val_size=0.25)
 
-    def train_model(self, steps, epochs, verbose_every=10):
-
+    def train_model(self, steps, epochs, validate_every=10):
         X_val = self.data_helper.X_val
         y_val = self.data_helper.y_val
         max_acc = 0.0
         for i in range(epochs):
             X_train_batch, y_train_batch = self.data_helper.get_next_batch(i)
             for j in range(steps):
-                _, loss_val, accuracy_val = self.sess.run([self.train, self.loss, self.accuracy],
+                _, loss_val, accuracy_val, confidence_val = self.sess.run([self.train, self.loss, self.accuracy, self.confidence],
                                                      feed_dict={
                                                          self.X: X_train_batch,
                                                          self.y_true: y_train_batch,
-                                                         self.keep_prob: 0.8})
+                                                         self.keep_prob: 0.8,
+                                                         self.is_training: True})
                 if self.verbose:
                     print("Training loss: {}, accuracy: {}".format(str(loss_val), str(accuracy_val)))
-
-            acc = self.sess.run(self.accuracy, feed_dict={self.X: X_val, self.y_true: y_val, self.keep_prob: 1.0})
-            if acc > max_acc:
-                self.save_model(self.sess, epochs)
-                max_acc = acc
-                if self.verbose:
-                    print("Better accuracy found. LSTM model saved")
-            if self.verbose:
+            if i % validate_every == 0:
+                acc = self.sess.run(self.accuracy, feed_dict={self.X: X_val,
+                                                              self.y_true: y_val,
+                                                              self.keep_prob: 1.0,
+                                                              self.is_training: False})
+                if acc > max_acc:
+                    self.save_model(self.sess)
+                    max_acc = acc
+                    if self.verbose:
+                        print("Better accuracy found. LSTM model saved")
                 print("----------------- Step {}: validation accuracy {} ----------------".format(str(i), str(acc)))
+            # else:
+            #     print("-----------------------------------------------------")
 
 
 
-
-    def save_model(self, sess, final_epoch):
-        saver = tf.train.Saver()
-        saver.save(sess, self.model_dir + "basic_lstm")
+    def save_model(self, sess):
+        saver = tf.train.Saver(save_relative_paths=True)
+        if not os.path.exists(self.model_dir + "basic_lstm.meta"):
+            save_meta = True
+        else: save_meta = False
+        saver.save(sess, self.model_dir + "basic_lstm", write_meta_graph=save_meta)
 
     def restore_model(self):
         saver = tf.train.import_meta_graph(self.model_dir+"basic_lstm.meta")
         saver.restore(self.sess, tf.train.latest_checkpoint(self.model_dir))
+        self.graph = tf.get_default_graph()
         self.X = self.graph.get_tensor_by_name("input/X:0")
         self.y_true = self.graph.get_tensor_by_name("input/y_true:0")
         self.keep_prob = self.graph.get_tensor_by_name("input/keep_prob:0")
+        self.is_training = self.graph.get_tensor_by_name("input/is_training:0")
+        self.scores = self.graph.get_tensor_by_name("scores_output/scores: 0")
+        self.confidence = self.graph.get_tensor_by_name("scores_output/confidence:0")
         self.loss = self.graph.get_tensor_by_name("cross_entropy/loss:0")
         self.accuracy = self.graph.get_tensor_by_name("accuracy/accuracy: 0")
         self.train = tf.get_collection("train")[0]
 
+    def predict(self, raw_text):
+        # x_b, _ = self.data_helper.get_next_batch(0)
+        # x_second = x_b[0: 3, :]
+        # x_third = x_b[0, :].reshape([1, -1])
+        # print(x_second.dtype)
+        # print()
+        # print( x_third.dtype)
+        # print(self.data_helper.X_train.shape)
+        # self.restore_model()
+        wd_list = self.data_helper.clean_sentence(raw_text)
+        # print("cleaned words list: ", wd_list)
+        wd_id_list = self.data_helper.corpora_dict.doc2idx(wd_list)
+        wd_id_padded = self.data_helper.pad_sequence([wd_id_list], self.num_wds)
+        # wd_id_padded_batch = np.stack([wd_id_padded for _ in range(64)]).reshape([64, 200])
+        # print("What is sent to tf", wd_id_padded)
+        # x_first = np.reshape(x_b[0, :], [1, 200])
+        # print(wd_id_padded_batch.dtype, x_b.dtype)
+        # print(type(wd_id_padded_batch), type(x_b))
+        # print(wd_id_padded_batch.shape, x_b.shape)
+
+        confidence_val = self.sess.run( self.confidence,
+                                        feed_dict={self.X: wd_id_padded,
+                                                   self.keep_prob: 1.0,
+                                                   self.is_training:False})
+        print(confidence_val)
